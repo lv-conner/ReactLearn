66e2b04bb075499ad96f0bb9c7679e8e4b243376
lv-conner <lvmh123456@gmail.com>
2018-09-12 16:58:22 +0800

    返回菜单数据

---
+++ Source.js
@@ -0,0 +1,39 @@
+var Source = {
+    menuItems: [{
+        text: "搜索网站",
+        subItems: [{
+                "id": "bing",
+                "url": "https://www.bing.com",
+                "text": "必应",
+                "active":false
+            },
+            {
+                "id": "baidu",
+                "url": "https://www.baidu.com",
+                "text": "百度",
+                "active":false
+            }
+        ]
+    },{
+        text:"主页",
+        subItems:[
+            {
+                id:"about",
+                url:"/Home/about",
+                text:"关于",
+                active:false
+            }
+        ]
+    },{
+        text:"报表",
+        subItems:[
+            {
+                id:"report",
+                url:"/report/index",
+                text:'报表',
+                active:false
+            }
+        ]
+    }]
+}
+module.exports = Source;
--- sampleServer.js
+++ sampleServer.js
@@ -1,15 +1,18 @@
 var http = require('http');
 var url = require('url');
+var source = require("./Source");
 
 // 创建服务器
 http.createServer(function (request, response) {
     response.writeHead(200, {
         'Content-Type': 'application/json',
         'Origin': '*',
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
+        "dataType":"application/json"
     });
     var options = [1, 3, 4, 5, 6, 7, 8, 8];
-    response.write(JSON.stringify(options));
+    var data = {"data":source.menuItems};
+    response.write(JSON.stringify(data));
     response.end();
 }).listen(13130);
 
